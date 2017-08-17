be6972c48a0d58ec48f0a24143ac95351c920ab7
edwardcooper <maxminicherrycc@gmail.com>
2017-08-17 18:35:58 -0400

    Add scraping codes for energies in log.lammps
    Add TotEng, PotEng, and KinEng scraping methods.
    change the file name after scraping from tvp.csv to log.csv

--- TvpExtract.sh
+++ TvpExtract.sh
@@ -3,14 +3,23 @@ if test -e log.lammps
 	then 
                 echo "Start scraping for relevent data."
 
-		grep "Temp" log.lammps | tr "T" "\n" | grep emp | replace emp Temp | replace "=" "" > tvp.csv
+		grep "Temp" log.lammps | tr "T" "\n" | grep emp | replace emp Temp | replace "=" "" > log.csv
 		echo "Finish scraping Temperature"
 
-		grep Volume log.lammps | tr "=" " " >> tvp.csv
+		grep Volume log.lammps | tr "=" " " >> log.csv
 		echo "Finish scraping volume."
 
-		grep "Press"  log.lammps | tr "P" "\n" | grep ress | replace ress Press | replace "=" "" >> tvp.csv
+		grep "Press"  log.lammps | tr "P" "\n" | grep ress | replace ress Press | replace "=" "" >> log.csv
                 echo "Finish scraping pressure." 
+		
+		grep "TotEng" log.lammps | tr "K" "\n" | grep "TotEng" | replace "=" "" >> log.csv
+		echo "Finish scraping total energy"
+		
+		grep "PotEng" log.lammps | replace "E_bond" "This" | tr "This" "\n" | grep "TotEng" | replace "=" "" >>log.csv 
+		echo "Finish scraping potential energy."
+
+		grep KinEng log.lammps | tr "Temp" "\n" | tr "K" "\n" | grep inEng | replace "inEng" "KinEng" | replace "=" "" >> log.csv 
+		echo "Finish scraping kinetic energy."
 	else 
 		echo "The file log.lammps is not here."
 fi
