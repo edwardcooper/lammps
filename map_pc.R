246dd8eaad43f99ea90a3ce9c02f45fe22c7e049
edwardcooper <maxminicherrycc@gmail.com>
2017-09-13 16:26:52 -0400

    write the stationary test into using parallel computation mode.

---
+++ map_pc.R
@@ -0,0 +1,32 @@
+## write the parallel version of map function map_c with foreach and doParallel. map_c means along column. 
+map_pc=function(data,FUN){
+  library(foreach)
+  library(doParallel)
+  
+  # make a fork cluster with half of the cores.
+  # Half of the cores are the number of real cores. 
+  cl=makeCluster(detectCores()/2,type="FORK")
+  registerDoParallel(cl)
+  
+  #result should return a list by default.
+  out_list=foreach(i=1:ncol(data))%dopar%{
+    result=FUN(data[,i])
+  }
+  message=paste("The computer is using ", getDoParWorkers(), " core(s).",sep="")
+  print(message)
+  # close the parallel ports 
+  gc()
+  stopCluster(cl)
+  stopImplicitCluster()
+  
+  return(out_list)
+}
+# example use 
+# random_data=data.frame(matrix(rnorm(1e8),ncol=10,nrow=1e7))
+#                                     
+# cumsum_result=map_p(data=random_data,sum)
+# 
+# cumsum_result%>%str
+
+
+
--- st_extract_cal.R
+++ st_extract_cal.R
@@ -1,39 +1,41 @@
 
 source('~/Dropbox/lammps/equilibrium_extract.R', echo=TRUE)
- 
+source('~/Dropbox/lammps/map_pc.R', echo=TRUE) 
+
+
 #library(beepr) # for sound effect.
+ library(purrr)
+ library(magrittr)
  log_data=read.table("log320_solomon.csv")
  log_data=log_data%>%long_to_wide()
  colnames(log_data)
- library(purrr)
- library(magrittr)
- st_test_result=log_data%>%map(function(data) st_extract_tc(data,method="fractal"))
+ st_test_result=log_data%>%map_pc(function(data) st_extract_tc(data,method="fractal"))
  st_test_result%>%map(length)
 
- 
- # library(purrr)
- # library(magrittr)
- # st_test_result=log_data%>%map(function(data) st_extract_tc(data,method="kpss"))
- # st_test_result
-log_data4=read.table("log600K.csv")
-log_data4=log_data4%>%long_to_wide()
-st_test_result8=log_data4%>%map(function(data) st_extract_tc(data,method="fractal",interval=10^5))
-st_test_result9=log_data4%>%map(function(data) st_extract_tc(data,method="kpss"))
 
-st_test_result8%>%map(length)
-st_test_result9%>%map(length)
-
-log_data3=read.table("log.csv")
-log_data3=log_data3%>%long_to_wide()
-colnames(log_data3)
-st_test_result3=log_data3%>%map(function(data) st_extract_tc(data,method="fractal",interval=10^6))
-#st_test_result4=log_data3%>%map(function(data) st_extract_tc(data,method="adf"))
-st_test_result5=log_data3%>%map(function(data) st_extract_tc(data,method="kpss"))
-#st_test_result6=log_data3%>%map(function(data) st_extract_tc(data,method="pp"))
-#st_test_result7=log_data3%>%map(function(data) st_extract_tc(data,method="stats.pp",interval=10^6))
-
-st_test_result3%>%map(length)
-st_test_result4%>%map(length)
-st_test_result5%>%map(length)
-st_test_result6%>%map(length)
-st_test_result7%>%map(length)
+#  # library(purrr)
+#  # library(magrittr)
+#  # st_test_result=log_data%>%map(function(data) st_extract_tc(data,method="kpss"))
+#  # st_test_result
+# log_data4=read.table("log600K.csv")
+# log_data4=log_data4%>%long_to_wide()
+# st_test_result8=log_data4%>%map(function(data) st_extract_tc(data,method="fractal",interval=10^5))
+# st_test_result9=log_data4%>%map(function(data) st_extract_tc(data,method="kpss"))
+# 
+# st_test_result8%>%map(length)
+# st_test_result9%>%map(length)
+# 
+# log_data3=read.table("log.csv")
+# log_data3=log_data3%>%long_to_wide()
+# colnames(log_data3)
+# st_test_result3=log_data3%>%map(function(data) st_extract_tc(data,method="fractal",interval=10^6))
+# #st_test_result4=log_data3%>%map(function(data) st_extract_tc(data,method="adf"))
+# st_test_result5=log_data3%>%map(function(data) st_extract_tc(data,method="kpss"))
+# #st_test_result6=log_data3%>%map(function(data) st_extract_tc(data,method="pp"))
+# #st_test_result7=log_data3%>%map(function(data) st_extract_tc(data,method="stats.pp",interval=10^6))
+# 
+# st_test_result3%>%map(length)
+# st_test_result4%>%map(length)
+# st_test_result5%>%map(length)
+# st_test_result6%>%map(length)
+# st_test_result7%>%map(length)
