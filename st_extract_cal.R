ae5aefb6cecead87efa7001a3df8455bd282a5c3
edwardcooper <maxminicherrycc@gmail.com>
2017-09-27 21:11:25 -0400

    Delete non-essential comments.

--- st_extract_cal.R
+++ st_extract_cal.R
@@ -102,29 +102,4 @@ timeRecordB(output_message="test_calculation core_num: 8")
 
 timeRecordR()%>%filter(output_message!="None")%>%select(output_message,run_time) 
 
-#  # library(purrr)
-#  # library(magrittr)
-#  # st_test_result=log_data%>%map(function(data) st_extract_tc(data,method="kpss"))
-#  # st_test_result
-# log_data4=read.table("log600K.csv")
-# log_data4=log_data4%>%long_to_wide()
-# st_test_result8=log_data4%>%map(function(data) st_extract_tc(data,method="kpss",interval=10^5))
-# st_test_result9=log_data4%>%map(function(data) st_extract_tc(data,method="kpss"))
-# 
-# st_test_result8%>%map(length)
-# st_test_result9%>%map(length)
-# 
-# log_data3=read.table("log.csv")
-# log_data3=log_data3%>%long_to_wide()
-# colnames(log_data3)
-# st_test_result3=log_data3%>%map(function(data) st_extract_tc(data,method="kpss",interval=10^6))
-# #st_test_result4=log_data3%>%map(function(data) st_extract_tc(data,method="adf"))
-# st_test_result5=log_data3%>%map(function(data) st_extract_tc(data,method="kpss"))
-# #st_test_result6=log_data3%>%map(function(data) st_extract_tc(data,method="pp"))
-# #st_test_result7=log_data3%>%map(function(data) st_extract_tc(data,method="stats.pp",interval=10^6))
-# 
-# st_test_result3%>%map(length)
-# st_test_result4%>%map(length)
-# st_test_result5%>%map(length)
-# st_test_result6%>%map(length)
-# st_test_result7%>%map(length)
+
